440ce805cd92c40b76989a91e5d145edf9671eb6
exercise13-11 <phuoclamthien@gmail.com>
2024-11-13 11:18:10 +0100

    Init

--- src/main/java/dk/easvAE/Main.java
+++ src/main/java/dk/easvAE/Main.java
@@ -4,5 +4,11 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("I git  some changes");
         System.out.println("hiii1234");
+        System.out.println("HelloColleagues");
+
+        int Phuoc = 9;
+        if (Phuoc == 9) {
+            System.out.println("Phuoc 9");
+        }
     }
 }
