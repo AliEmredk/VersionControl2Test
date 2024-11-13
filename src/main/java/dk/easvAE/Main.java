d90cdea64c0eed94369b24c1dfa352f191bccbeb
Thien Phuoc Lam <phuoclamthien@gmail.com>
2024-11-13 11:12:47 +0100

    Minor changes

--- src/main/java/dk/easvAE/Main.java
+++ src/main/java/dk/easvAE/Main.java
@@ -3,6 +3,6 @@ package dk.easvAE;
 public class Main {
     public static void main(String[] args) {
         System.out.println("I git  some changes");
-        System.out.println("hiii");
+        System.out.println("hiii1234");
     }
 }
