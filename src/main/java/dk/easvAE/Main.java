251ecf2b590e76a6921e5fd794b04f2331fc4b14
exercise13-11 <phuoclamthien@gmail.com>
2024-11-13 11:18:10 +0100

    Init

--- src/main/java/dk/easvAE/Main.java
+++ src/main/java/dk/easvAE/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("I git  some changes");
         System.out.println("hiii1234");
+        System.out.println("HelloColleagues");
     }
 }
