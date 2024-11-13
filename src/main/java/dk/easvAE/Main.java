24945416dffe7fbca0029a2ca0bd2ec9f42a5c3c
Thien Phuoc Lam <phuoclamthien@gmail.com>
2024-11-13 11:09:13 +0100

    My new commit

--- src/main/java/dk/easvAE/Main.java
+++ src/main/java/dk/easvAE/Main.java
@@ -2,7 +2,7 @@ package dk.easvAE;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("I made some changes");
+        System.out.println("I git  some changes");
         System.out.println("hiii");
     }
 }
