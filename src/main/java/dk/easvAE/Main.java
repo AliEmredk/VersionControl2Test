a1d104a53a9ea97c9156f5408f6770826abb4006
Emre <aliemreuzunoglu@gmail.com>
2024-11-13 10:43:41 +0100

    git commit -m<I'm going home> git push

---
+++ src/main/java/dk/easvAE/Main.java
@@ -0,0 +1,8 @@
+package dk.easvAE;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("I made some changes");
+        System.out.println("hiii");
+    }
+}
